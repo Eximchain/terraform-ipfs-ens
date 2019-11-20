034a58560f737c2cf301f9ca2966becc217d77cc
John O'Sullivan <john@eximchain.com>
2019-11-20 17:48:05 -0500

    Two new vars, indented CodeBuild project

--- main.tf
+++ main.tf
@@ -400,6 +400,8 @@ terraform {
       REACT_APP_IPFS_ENS_API_URL          = "https://${local.api_domain}"
       REACT_APP_WEB3_URL                  = "https://gamma-tx-executor-us-east.eximchain-dev.com"
       REACT_APP_SEGMENT_BROWSER_WRITE_KEY = var.segment_browser_write_key
+      REACT_APP_OAUTH_CLIENT_ID           = var.github_client_id
+      REACT_APP_OAUTH_CLIENT_SECRET       = var.github_client_secret
     }
   }
 
@@ -445,24 +447,24 @@ terraform {
 # ---------------------------------------------------------------------------------------------------------------------
 # CODEBUILD PROJECT
 # ---------------------------------------------------------------------------------------------------------------------
-resource "aws_codebuild_project" "ipfs_builder" {
-  name          = "ipfs-builder-${var.subdomain}"
-  build_timeout = 10
-  service_role  = aws_iam_role.ipfs_ens_codepipeline_iam.arn
+  resource "aws_codebuild_project" "ipfs_builder" {
+    name          = "ipfs-builder-${var.subdomain}"
+    build_timeout = 10
+    service_role  = aws_iam_role.ipfs_ens_codepipeline_iam.arn
 
-  environment {
-    type         = "LINUX_CONTAINER"
-    compute_type = "BUILD_GENERAL1_SMALL"
-    image        = "aws/codebuild/standard:2.0"
-  }
+    environment {
+      type         = "LINUX_CONTAINER"
+      compute_type = "BUILD_GENERAL1_SMALL"
+      image        = "aws/codebuild/standard:2.0"
+    }
 
-  artifacts {
-    type                = "CODEPIPELINE"
-    encryption_disabled = true
-  }
+    artifacts {
+      type                = "CODEPIPELINE"
+      encryption_disabled = true
+    }
 
-  source {
-    type      = "CODEPIPELINE"
-    buildspec = file("${path.module}/buildspec.yml")
+    source {
+      type      = "CODEPIPELINE"
+      buildspec = file("${path.module}/buildspec.yml")
+    }
   }
-}
